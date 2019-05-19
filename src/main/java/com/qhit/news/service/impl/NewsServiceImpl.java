f8e1f73dc277eb7734717b9a44c5d20986ab38e2
Mr.wang <34223819+git-Mr-wang@users.noreply.github.com>
2019-05-19 21:01:56 +0800

    Update NewsServiceImpl.java

--- src/main/java/com/qhit/news/service/impl/NewsServiceImpl.java
+++ src/main/java/com/qhit/news/service/impl/NewsServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource; 
 
 /**
-* Created by GeneratorCode on 2019/05/08
+* Created by GeneratorCode on 2018/12/11
 */
 
 @Service 
